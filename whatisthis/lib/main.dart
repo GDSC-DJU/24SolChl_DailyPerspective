c1b49df95812a88db21deca19a474d2f3612d7f2
원형 <blue4509as@gmail.com>
2024-01-31 20:34:03 +0900

    재형버튼 추가

--- whatisthis/lib/main.dart
+++ whatisthis/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
-import 'package:flutter/services.dart';
-import 'package:flutter_in_app_pip/flutter_in_app_pip.dart';
-import 'package:whatisthis/generate/generate_text.dart';
->>>>>>> ac4bd5e6ac16f475c373a1c316952239759c236d
 import 'package:whatisthis/screen/mainscreen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -14,21 +7,3 @@ void main() async {
   //generate();
   runApp(const MyApp());
 }
-<<<<<<< HEAD
-=======
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return PiPMaterialApp(
-      title: 'imageTTs',
-      theme: ThemeData(
-          appBarTheme:
-              const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.dark)),
-      home: const mainscreen(),
-    );
-  }
-}
->>>>>>> ac4bd5e6ac16f475c373a1c316952239759c236d
