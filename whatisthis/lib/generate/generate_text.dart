a82be9bc5c1c6c21a050f16d118173bb81f2b07b
재형 <chamchi0129@gmail.com>
2024-02-07 20:38:46 +0900

    .vscode gitignore

--- whatisthis/lib/generate/generate_text.dart
+++ whatisthis/lib/generate/generate_text.dart
@@ -3,21 +3,25 @@ import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:whatisthis/screenshot/screenshot.dart';
 
 generate() async {
+  captureScreenshot();
   Gemini.init(apiKey: dotenv.get("key"));
   final gemini = Gemini.instance;
 
   Uint8List bytes =
       (await rootBundle.load('assets/test.png')).buffer.asUint8List();
+  //이후 assets/screenshot.png 로 교체
 
   final result = await gemini.textAndImage(
     text: "Explain this image in Korean.",
     images: [bytes],
   );
 
-  log(result?.content?.parts?.last.text ?? '');
+  //log(result?.content?.parts?.last.text ?? '');
   tts(result?.content?.parts?.last.text ?? '');
+  //이후 사용 이미지 삭제
 }
 
 void tts(String text) async {
--- whatisthis/lib/screenshot/screenshot.dart
+++ whatisthis/lib/screenshot/screenshot.dart
@@ -3,22 +3,20 @@ import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
 import 'dart:io';
 
-  final ScreenshotController screenshotController = ScreenshotController();
+final ScreenshotController screenshotController = ScreenshotController();
 
-Future<void> _captureScreenshot() async {
-    try {
-      // Screenshot 캡처
-      Uint8List? imageUint8List = await screenshotController.capture();
+Future<void> captureScreenshot() async {
+  try {
+    // Screenshot 캡처
+    Uint8List? imageUint8List = await screenshotController.capture();
 
-      // 이미지 데이터를 파일로 저장
-      Directory appDocDir = await getApplicationDocumentsDirectory();
-      String appDocPath = appDocDir.path;
-      String filePath = '$appDocPath/screenshot.png';
-      File file = File(filePath);
-      await file.writeAsBytes(imageUint8List!);
-
-      print('Screenshot captured: $filePath');
-    } catch (e) {
-      print('Error capturing screenshot: $e');
-    }
+    // 이미지 데이터를 파일로 저장
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    String filePath = '$appDocPath/screenshot.png';
+    File file = File(filePath);
+    await file.writeAsBytes(imageUint8List!);
+  } catch (e) {
+    //에러
   }
+}
